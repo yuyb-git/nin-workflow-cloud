e74ddf08307a88cdd21ed47437f5ba174c0dde54
yyb881109 <yyb881109@.github.com>
2021-01-08 14:27:47 +0800

    解决zipkin启动报错

---
+++ nin-zuul/src/main/java/cn/netinnet/ninzuul/config/SleuthSamplerConfiguration.java
@@ -0,0 +1,31 @@
+package cn.netinnet.ninzuul.config;
+
+import brave.sampler.Sampler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.sleuth.sampler.ProbabilityBasedSampler;
+import org.springframework.cloud.sleuth.sampler.SamplerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName SleuthSamplerConfiguration
+ * @Description
+ * @Author yuyb
+ * @Date 2021/1/8 14:21
+ */
+@Configuration
+public class SleuthSamplerConfiguration {
+
+    @Value("${spring.sleuth.sampler.probability}")
+    private String probability;
+
+    @Bean
+    public Sampler defaultSampler() throws Exception {
+        Float f = new Float(probability);
+        SamplerProperties samplerProperties = new SamplerProperties();
+        samplerProperties.setProbability(f);
+        ProbabilityBasedSampler sampler = new ProbabilityBasedSampler(samplerProperties);
+        return sampler;
+    }
+
+}
